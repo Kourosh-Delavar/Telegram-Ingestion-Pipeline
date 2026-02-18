b1f716073cefb82b185563613d9bedb7be820a10
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-02-18 10:23:12 +0100

    Adding JSON file, containing paths for file saving.

--- src/tg_ingestion_pipeline/loading/save_media_files.py
+++ src/tg_ingestion_pipeline/loading/save_media_files.py
@@ -1,7 +1,7 @@
-from typing import Optional, Dict
+from typing import Dict
 from telegram import Update, Message
 from telegram.ext import ContextTypes
-
+import json
 
 async def save_media_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -12,6 +12,14 @@ async def save_media_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -
     :param context: The Telegram context object
     :type context: ContextTypes.DEFAULT_TYPE
     """
+
+    # Loading paths from JSON file
+    try:
+        with open("test/loading_test/test_saving_paths.json", "r") as f:
+                paths: Dict[str, str] = json.load(f)
+    except FileNotFoundError:
+        print("Error: test_saving_paths.json not found.")
+        return # TODO: replace with logging and raise an exception
 
     msg: Optional[Message] = update.message
     if not msg:
@@ -24,7 +32,7 @@ async def save_media_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -
         file  = await context.bot.get_file(file_id)
         extension = msg.photo[-1].mime_type.split("/")[-1].lower()
 
-        await file.download_to_drive(f"{load_paths["photos"]}/{file_id}.{extension}")
+        await file.download_to_drive(f"{paths['photos']}/{file_id}.{extension}")
 
     # Check if the message contains a document (PDF, DOCX, TXT)
     elif msg.document:
@@ -33,7 +41,7 @@ async def save_media_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -
         file  = await context.bot.get_file(file_id)
         extension = msg.document.mime_type.split("/")[-1].lower()
 
-        await file.download_to_drive(f"{load_paths()['documents']}/{extension}/{file_id}.{extension}")
+        await file.download_to_drive(f"{paths['documents'][extension]}/{file_id}.{extension}")
     
     # Check if the message contains an audio file (music or voice message)
     elif msg.audio or msg.voice:
@@ -46,4 +54,4 @@ async def save_media_files(update: Update, context: ContextTypes.DEFAULT_TYPE) -
         else:
             subdir = "music"
 
-        await file.download_to_drive(f"{load_paths()['audio']}/{subdir}/{file_id}.{extension}")  
+        await file.download_to_drive(f"{paths['audio'][subdir]}/{file_id}.{extension}")  
