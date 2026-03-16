4030a756cf17f65c17752167aef74ffce643859b
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-03-16 14:54:39 +0100

    Rename: Rename json_flatten.py -> data_flatten.py, json_loader.py -> data_loader.py

---
+++ src/tg_ingestion_pipeline/transformation/processing/data_flatten.py
@@ -0,0 +1,4 @@
+"""
+Data flatten module: Contains the DataFlattener class responsible for flattening nested data structures in the deserialized messages.
+"""
+
---
+++ src/tg_ingestion_pipeline/transformation/processing/data_loader.py
@@ -0,0 +1,6 @@
+"""
+Data loader module: Consume serialized messages from Kafka topic, deserialize them using the provided Avro schema and yield the deserialized messages for further processing in the pipeline.  
+"""
+
+
+
