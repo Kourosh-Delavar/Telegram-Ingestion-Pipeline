7a75629cad2231fead3b5bcba7c778d7596261fd
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-03-20 11:39:15 +0100

    bug: Add commit after execution. Closing cursor and connection after the commit

--- src/tg_ingestion_pipeline/loading/db/init_db.py
+++ src/tg_ingestion_pipeline/loading/db/init_db.py
@@ -1,10 +1,9 @@
 import logging
 from typing import Optional
-import psycopg2
-from psycopg2 import sql
-from psycopg2.extras import execute_batch
+# from psycopg2.extras import execute_batch
 from pathlib import Path
-from connect_db import get_connection
+from .connect_db import get_connection
+from .utils.get_sql_file import get_sql
 
 # Configure logging
 logging.basicConfig(
@@ -14,28 +13,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Path to SQL file used for initializing the database 
-SQL_PATH = "./db/postgres/schema/001_create_tables.sql"
+INIT_SQL_PATH = "./db/postgres/schema/001_create_tables.sql"
 
-def get_sql(sql_path: str) -> Optional[str]:
-    """
-    Loads SQL scripts from the given path
-
-    :param sql_path: Path to the sql file
-    :type sql_path: str
-    :return: str | None
-    """
-
-    try:
-        logger.info(f"Loading SQL file from {SQL_PATH}")
-        with open(SQL_PATH, 'r', encoding='utf-8') as f:
-            sql = f.read()
-            if sql is not None:
-                logger.info(f"SQL file successfully loaded from {SQL_PATH}")
-                return sql
-    except Exception as e:
-        logger.error(f"Error loading SQL file from {SQL_PATH}: {e}")
-        return None
-    
 def initialize_db(conn) -> None:
     """
     Executes SQL files using connection object to initialize the database
@@ -47,12 +26,19 @@ def initialize_db(conn) -> None:
 
     try:
         with conn.cursor() as curs:
-            sql = get_sql(SQL_PATH)
+            sql = get_sql(INIT_SQL_PATH)
             curs.execute(sql)
-        logger.info("Database initialized successfully")
+            curs.commit()
+            logger.info("Database initialized successfully")
     except Exception as e:
+        conn.rollback()
         logger.error(f"Error initializing database: {e}")
         raise
+    finally:
+        curs.close()
+        conn.close()
 
 if __name__ == "__main__":
     initialize_db()
+
+# TODO: Add batch execution capability using execute_batch
