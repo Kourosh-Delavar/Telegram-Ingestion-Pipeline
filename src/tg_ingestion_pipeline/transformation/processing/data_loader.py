0bce10c590d272fc6603c33f3d5a147c207639df
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-04-01 11:11:30 +0200

    refactor: remove deserializer and JSON parser (already implemented in kafka engine consumer function)

--- src/tg_ingestion_pipeline/transformation/processing/data_loader.py
+++ src/tg_ingestion_pipeline/transformation/processing/data_loader.py
@@ -1,9 +1,5 @@
-"""
-Data loader module: Consume serialized messages from Kafka topic, deserialize them using the provided Avro schema and yield the deserialized messages for further processing in the pipeline.  
-"""
-
 import logging 
-from typing import Dict, Any
+from typing import Dict, Any, AsyncGenerator
 import json
 from kafka.kafka_engine import KafkaOrchestrator
 
@@ -11,15 +7,15 @@ from kafka.kafka_engine import KafkaOrchestrator
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-async def load_data_from_kafka(topic: str, group_id: str) -> Dict[str, Any]:
+async def load_data_from_kafka(topic: str, group_id: str) -> AsyncGenerator[Dict[str, Any], None]:
     """
-    Consume message from kafka topic and yield the deserialized message
+    Consume message from kafka topic and yield the message. 
 
     :param topic: the kafka topic from which messages should be consumed
     :type topic: str
     :param group_id: the consumer group ID 
     :type group_id: str
-    :return: Generator[Dict[str, Any], None, None]
+    :return: AsyncGenerator[Dict[str, Any], None] 
     """
 
     try:
@@ -27,19 +23,7 @@ async def load_data_from_kafka(topic: str, group_id: str) -> Dict[str, Any]:
         for message in orchestrator.consume_message(topic, group_id):
             if message is not None:
                 logging.info(f"Message consumed from topic {topic} and ")
-                # Deserialize the message 
-                try:
-                    deserialized_message: str = message.decode('utf-8')
-                    logging.info(f"Message deserialized successfully")
-                except Exception as e:
-                    logging.error(f"Failed to deserialize message: {e}")
-                # Parse to JSON
-                try:
-                    json_message: Dict[str, Any] = json.loads(deserialized_message)
-                    logging.info(f"Message parsed to JSON successfully")
-                    yield json_message
-                except Exception as e:
-                    logging.error(f"Failed to parse message to JSON: {e}")
+                yield message
             else:
                 logging.warning(f"Received empty message from topic {topic}")
     except Exception as e:
