3d15ba4838422d30aec1013a606f3cf7a09ebe12
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-04-01 11:40:40 +0200

    docs: improve DataFlattener docstring

--- src/tg_ingestion_pipeline/transformation/processing/data_flatten.py
+++ src/tg_ingestion_pipeline/transformation/processing/data_flatten.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Generator
+from typing import AsyncGenerator, Dict, Any, Generator
 import logging 
 
 # Configure logging
@@ -7,19 +7,17 @@ logger = logging.getLogger(__name__)
 
 class DataFlattener:
     """
-    DataFlattener is a class responsible for flattening nested data structures in the deserialized messages.
+    DataFlattener flattens nested data structures to simplify their use in downstream processes.
+    The flattened data is mainly used as a context component for vector database operations.
     """
 
-    def __init__(self):
-        pass        
-
-    def _flatten_dict(self, d: Dict[str, Any], parent_key):
+    def _flatten_dict(self, d: AsyncGenerator[Dict[str, Any], None], parent_key):
         """
         Flatten a nested dictionary by concatenating keys with a separator.
         """
         pass
 
-    def flatten_data(self, data: Generator[Dict[str, Any], None, None]) -> Generator[str, None, None]:
+    def flatten_data(self, data: AsyncGenerator[Dict[str, Any], None]) -> Generator[str, None, None]:
         """
         Flatten nested data structures in the deserialized messages.
 
