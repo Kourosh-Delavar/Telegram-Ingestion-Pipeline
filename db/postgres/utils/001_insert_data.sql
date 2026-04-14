bf293b33bee9419fb319aaba58102094ffeb900c
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-04-14 23:06:43 +0200

    fix: fix for table insertion

--- db/postgres/utils/001_insert_data.sql
+++ db/postgres/utils/001_insert_data.sql
@@ -10,20 +10,20 @@ INSERT INTO messages (
     file_id,
     mime_type,
     file_name,
-    duration_seconds,
+    duration_seconds
 )
 VALUES (
-    {message_id},
-    {message_type},
-    {message_timestamp},
-    {chat_id},
-    {content},
-    {user_id},
-    {username},
-    {reply_to},
-    {file_id},
-    {mime_type},
-    {file_name},
-    {duration_seconds}
+    %(message_id)s,
+    %(message_type)s,
+    %(message_timestamp)s,
+    %(chat_id)s,
+    %(content)s,
+    %(user_id)s,
+    %(username)s,
+    %(reply_to)s,
+    %(file_id)s,
+    %(mime_type)s,
+    %(file_name)s,
+    %(duration_seconds)s
 )
 ON CONFLICT (chat_id, message_id) DO NOTHING;
