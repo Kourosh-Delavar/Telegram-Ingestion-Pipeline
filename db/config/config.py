f056592b0843703f39c41c34b0536a95478a5e8a
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-04-23 11:46:51 +0200

    refactor: get credentials from .env.postgres

--- db/config/config.py
+++ db/config/config.py
@@ -4,11 +4,11 @@ import os
 load_dotenv('.env.postgres')
  
 DB_CONFIG = {
-    "host": os.getenv("POSTGRES_HOST"),
-    "port": os.getenv("POSTGRES_PORT"),
-    "user": os.getenv("POSTGRES_USER"),
-    "password": os.getenv("POSTGRES_PASSWORD"),
-    "database": os.getenv("POSTGRES_DB")
+    "host": os.getenv("POSTGRES_HOST", "postgres"),  # Default to 'postgres' for Docker
+    "port": int(os.getenv("POSTGRES_PORT", 5432)),
+    "user": os.getenv("POSTGRES_USER", "test_user"),
+    "password": os.getenv("POSTGRES_PASSWORD", "123456789"),
+    "database": os.getenv("POSTGRES_DB", "test_db")
 }
  
 TABLE_NAME = "messages"
